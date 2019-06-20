331d959bddf59b7e04cc84f55c3be6d057942f17
AlexanderGolys <szyba71830>
2019-06-21 01:50:07 +0200

    checks who is winning

--- main.py
+++ main.py
@@ -366,8 +366,87 @@ def draw_pot(pot):
     pygame.display.update()
 
 
-def winning(table, players):
-    pass
+def winning(players, table):
+    p_list = players[:]
+
+    for i in range(len(players)):
+        for player in p_list:
+            if whos_better(table, p_list[i], player) == 2:
+                del p_list[0]
+                break
+    return p_list
+
+
+def whos_better(table, player1, player2):
+    ev1 = evaluate(table, player1)
+    ev2 = evaluate(table, player2)
+    if ev1[0] > ev2[0]:
+        return 0
+    if ev1[0] < ev2[0]:
+        return 2
+    if ev1[0] == 8 or ev1[0] == 7 or ev1[0] == 0:  # straight flush, four of a kind, straight
+        if ev1[2] > ev2[2]:
+            return 0
+        elif ev1[2] < ev2[2]:
+            return 2
+        else:
+            return 1
+    if ev1[0] == 6:
+        if ev1[2] > ev2[2]:
+            return 0
+        elif ev1[2] < ev2[2]:
+            return 2
+        else:
+            if ev1[3] > ev2[3]:
+                return 0
+            elif ev1[3] < ev2[3]:
+                return 2
+            else:
+                return 1
+    if ev1[0] == 5 or ev1[0] == 0:
+        ev1[1].sort(key=lambda c: c.value)
+        ev2[1].sort(key=lambda c: c.value)
+        while ev1 != []:
+            if ev1[-1] > ev2[-1]:
+                return 0
+            if ev1[-1] < ev2[-1]:
+                return 0
+            del ev1[-1]
+            del ev2[-1]
+        return 1
+    if ev1[0] == 3 or ev1[0] == 2:
+        if ev1[2] > ev2[2]:
+            return 0
+        elif ev1[2] < ev2[2]:
+            return 2
+        else:
+            if ev1[3] > ev2[3]:
+                return 0
+            elif ev1[3] < ev2[3]:
+                return 2
+            else:
+                if ev1[4] > ev2[4]:
+                    return 0
+                elif ev1[4] < ev2[4]:
+                    return 2
+                else:
+                    return 1
+    if ev1[0] == 1:
+        if ev1[2] > ev2[2]:
+            return 0
+        elif ev1[2] < ev2[2]:
+            return 2
+        else:
+            ev1[1].sort(key=lambda c: c.value)
+            ev2[1].sort(key=lambda c: c.value)
+            while ev1:
+                if ev1[1][-1] > ev2[1][-1]:
+                    return 0
+                if ev1[1][-1] < ev2[1][-1]:
+                    return 0
+                del ev1[1][-1]
+                del ev2[1][-1]
+            return 1
 
 
 def decode_for_winning(cards):
@@ -420,9 +499,13 @@ def evaluate(table, player):
             four = True
             c_four = [ev_cards[i], ev_cards[i+1], ev_cards[i+2], ev_cards[i+3]]
     for i in range(len(ev_cards) - 4):
-        if ev_cards[i].value == ev_cards[i+1].value - 1 == ev_cards[i+2].value - 2 == ev_cards[i+3].value - 3 == ev_cards[i+4].value - 4:
+        temp = ev_cards
+        for i in range(len(temp) - 1):
+            if temp[i].value == temp[i + 1].value:
+                del temp[i]
+        if temp[i].value == temp[i+1].value - 1 == temp[i+2].value - 2 == temp[i+3].value - 3 == temp[i+4].value - 4:
             streight = True
-            c_streight = [ev_cards[i], ev_cards[i+1], ev_cards[i+2], ev_cards[i+3], ev_cards[i+4]]
+            c_streight = [temp[i], temp[i+1], temp[i+2], temp[i+3], temp[i+4]]
             c_streight.sort(key=lambda c: c.color)
             if c_streight[0].color == c_streight[4].color:
                 s_flush = True
@@ -435,28 +518,29 @@ def evaluate(table, player):
             flush = True
             c_flush = [ev_cards[i], ev_cards[i+1], ev_cards[i+2], ev_cards[i+3], ev_cards[i+4]]
 
-
     ev_cards.sort(key=lambda c: c.value, reverse=True)
 
     if s_flush:
-        return 8, c_s_flush
+        return 8, c_s_flush, c_s_flush[-1].value
     elif four:
         for card in ev_cards:
             if card not in c_four:
                 c_four.append(card)
                 break
-        return 7, c_four
+        return 7, c_four, c_four[-1]
     elif full:
         if len(c_full) == 5:
-            return 6, c_full
+            return 6, c_full, max(set(c_full), key=c_full.count), min(set(c_full), key=c_full.count)
         c_full.sort(key=lambda c: c.value)
         if c_full[0] != c_full[2]:
-            return 6, c_full[2:]
-        return 6, c_full[:3] + c_full[5:]
+            c_full = c_full[2:]
+            return 6, c_full, max(set(c_full), key=c_full.count), min(set(c_full), key=c_full.count)
+        c_full = c_full[:3] + c_full[5:]
+        return 6, c_full, max(set(c_full), key=c_full.count), min(set(c_full), key=c_full.count)
     elif flush:
         return 5, c_flush[:6]
     elif streight:
-        return 4, c_streight
+        return 4, c_streight, c_streight[-1]
     elif trip:
         for card in ev_cards:
             if card not in c_trip:
@@ -466,7 +550,7 @@ def evaluate(table, player):
             if card not in c_trip:
                 c_trip.append(card)
                 break
-        return 3, c_trip
+        return 3, c_trip, c_trip[0], max(c_trip[3], c_trip[4]), min(c_trip[3], c_trip[4])
     elif t_pair:
         c_t_pair.sort(key=lambda c: c.value, reverse=True)
         c_t_pair = c_t_pair[:5]
@@ -474,7 +558,7 @@ def evaluate(table, player):
             if card not in c_t_pair:
                 c_t_pair.append(card)
                 break
-        return 2, c_t_pair
+        return 2, c_t_pair, max(c_t_pair[0], c_t_pair[2]), min(c_t_pair[0], c_t_pair[2]), c_t_pair[4]
     elif pair:
         for card in ev_cards:
             if card not in c_pair:
@@ -488,7 +572,7 @@ def evaluate(table, player):
             if card not in c_pair:
                 c_pair.append(card)
                 break
-        return 1, c_pair
+        return 1, c_pair, c_pair[0]
     else:
         return 0, ev_cards[:6]
 
